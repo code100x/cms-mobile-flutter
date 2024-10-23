90f292cd1839e37bb52d40f6351a0bceebe8cac1
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-23 22:16:13 +0530

    add the basic needed widgets for the home screen

--- lib/src/features/home/presentation/home_screen.dart
+++ lib/src/features/home/presentation/home_screen.dart
@@ -4,9 +4,77 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 6.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flexible(
+                    flex: 3,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              // TODO: replace with logo image later
+                              CircleAvatar(
+                                backgroundColor: Colors.blue,
+                                radius: 5,
+                              ),
+                              // TODO: style the text
+                              Text('100xdevs'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )),
+                Flexible(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      RichText(
+                          text: TextSpan(
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                        text: "Be a 100xDev because 10x isn't enough",
+                      )),
+                      const Text(
+                          'Unlock your potential with coding cohorts. Learn, collaborate, and grow beyond limits.'),
+                      ElevatedButton(
+                          onPressed: () => {},
+                          style: const ButtonStyle(
+                            backgroundColor:
+                                WidgetStatePropertyAll(Colors.blue),
+                          ),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                      OutlinedButton(
+                          style: ButtonStyle(),
+                          onPressed: () => {},
+                          child: const Text(
+                            'Sign Up Now',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
