ccf3e24d280c8df96de720bf7f6dfc3424ee38ca
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-24 22:10:32 +0530

    add some styles to app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -10,7 +10,15 @@ class MainApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final goRouter = ref.watch(goRouterProvider);
     return MaterialApp.router(
+      color: const Color(0xFF3B58E0),
+      theme: ThemeData.light(
+        useMaterial3: true,
+      ).copyWith(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF3B58E0)),
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.white)),
       routerConfig: goRouter,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
