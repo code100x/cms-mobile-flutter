c891b9ba7de6f3032d5fbf7d479e64046e6b82b5
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-25 18:50:43 +0530

    add the routing logic

--- lib/src/routing/router.dart
+++ lib/src/routing/router.dart
@@ -1,12 +1,131 @@
-import 'package:cms_flutter/src/features/home/presentation/home_screen.dart';
+import 'package:cms_flutter/src/features/account/presentation/account_screen.dart';
+import 'package:cms_flutter/src/features/account/presentation/change_password_screen.dart';
+import 'package:cms_flutter/src/features/account/presentation/edit_profile_screen.dart';
+import 'package:cms_flutter/src/features/auth/presentation/signin_screen.dart';
+import 'package:cms_flutter/src/features/auth/presentation/signup_screen.dart';
+import 'package:cms_flutter/src/features/auth/presentation/verify_otp_screen.dart';
+import 'package:cms_flutter/src/features/bookmarks/presentation/bookmarks_screen.dart';
+import 'package:cms_flutter/src/features/courses/presentation/course_week_screen.dart';
+import 'package:cms_flutter/src/features/courses/presentation/courses_screen.dart';
+import 'package:cms_flutter/src/features/courses/presentation/single_course_screen.dart';
+import 'package:cms_flutter/src/features/courses/video_player/presentation/video_player_screen.dart';
+import 'package:cms_flutter/src/features/downloads/presentation/downloads_screen.dart';
+import 'package:cms_flutter/src/features/history/presentation/history_screen.dart';
+import 'package:cms_flutter/src/features/landing/presentation/landing_screen.dart';
+import 'package:cms_flutter/src/routing/scaffold_with_navigation_bar.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+
+final _coursesShellKey = GlobalKey<NavigatorState>(debugLabel: 'coursesShell');
+final _downloadsShellKey =
+    GlobalKey<NavigatorState>(debugLabel: 'downloadsShell');
+final _bookmarksShellKey =
+    GlobalKey<NavigatorState>(debugLabel: 'bookmarksShell');
+final _historyShellKey = GlobalKey<NavigatorState>(debugLabel: 'historyShell');
+final _accountShellKey = GlobalKey<NavigatorState>(debugLabel: 'accountShell');
+
 final goRouterProvider = Provider<GoRouter>((ref) {
-  return GoRouter(routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
-    )
-  ]);
+  return GoRouter(
+    initialLocation: '/landing',
+    navigatorKey: _rootNavigatorKey,
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        path: '/landing',
+        name: 'landing',
+        pageBuilder: (context, state) =>
+            const NoTransitionPage(child: LandingScreen()),
+      ),
+      GoRoute(
+        path: '/signup',
+        name: 'signup',
+        pageBuilder: (context, state) =>
+            const NoTransitionPage(child: SignupScreen()),
+      ),
+      GoRoute(
+        path: '/signin',
+        name: 'signin',
+        pageBuilder: (context, state) =>
+            const NoTransitionPage(child: SigninScreen()),
+      ),
+      GoRoute(
+        path: '/verify-otp',
+        builder: (context, state) => const VerifyOtpScreen(),
+      ),
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) {
+          return ScaffoldWithNavigationBar(navigationShell: navigationShell);
+        },
+        branches: [
+          StatefulShellBranch(navigatorKey: _coursesShellKey, routes: [
+            GoRoute(
+                path: '/courses',
+                pageBuilder: (context, state) => const NoTransitionPage(
+                      child: CoursesScreen(),
+                    ),
+                routes: [
+                  GoRoute(
+                      path: ':courseId',
+                      builder: (context, state) => const SingleCourseScreen(),
+                      routes: [
+                        GoRoute(
+                            path: ':courseWeek',
+                            builder: (context, state) =>
+                                const CourseWeekScreen(),
+                            routes: [
+                              GoRoute(
+                                path: ':videoId',
+                                builder: (context, state) =>
+                                    const VideoPlayerScreen(),
+                              )
+                            ])
+                      ])
+                ])
+          ]),
+          StatefulShellBranch(navigatorKey: _downloadsShellKey, routes: [
+            GoRoute(
+              path: '/downloads',
+              pageBuilder: (context, state) =>
+                  const NoTransitionPage(child: DownloadsScreen()),
+            )
+          ]),
+          StatefulShellBranch(navigatorKey: _bookmarksShellKey, routes: [
+            GoRoute(
+              path: '/bookmarks',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: BookmarksScreen(),
+              ),
+            )
+          ]),
+          StatefulShellBranch(navigatorKey: _historyShellKey, routes: [
+            GoRoute(
+              path: '/history',
+              pageBuilder: (context, state) =>
+                  const NoTransitionPage(child: HistoryScreen()),
+            )
+          ]),
+          StatefulShellBranch(navigatorKey: _accountShellKey, routes: [
+            GoRoute(
+                path: '/account',
+                pageBuilder: (context, state) => const NoTransitionPage(
+                      child: AccountScreen(),
+                    ),
+                routes: [
+                  GoRoute(
+                    path: 'profile',
+                    builder: (context, state) => const EditProfileScreeen(),
+                  ),
+                  GoRoute(
+                    path: 'change-password',
+                    builder: (context, state) => const ChangePasswordScreen(),
+                  )
+                ])
+          ]),
+        ],
+      )
+    ],
+  );
 });
